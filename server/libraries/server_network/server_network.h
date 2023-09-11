3dbe5d22d74122a523a9926497fad467a3913065
root <root@CRISTIAN-LAPTOP>
2023-09-11 14:02:47 +0300

    modified the structure so that it accepts clients with one command

--- server/libraries/server_network/server_network.h
+++ server/libraries/server_network/server_network.h
@@ -5,6 +5,8 @@
 #ifndef SERVER_SERVER_NETWORK_H
 #define SERVER_SERVER_NETWORK_H
 
+#include <stdbool.h>
+
 typedef void (*LogMessageFunctionPtr)(const char* message);
 typedef void (*LogMessageFromPatternFunctionPtr)(const char* message, ...);
 
@@ -21,6 +23,14 @@ struct ILogger
 
 } NETWORK_LOGGER;
 
+enum OperationStatus
+{
+    FAILED,
+    SUCCEEDED
+};
+
+enum OperationStatus NETWORK_OPERATION_STATUS;
+
 int SERVER_SOCKET;
 
 void InitLogger( LogMessageFunctionPtr LogInfo,
@@ -33,6 +43,8 @@ void CreateServerSocket();
 void BindServerSocket();
 
 void OpenPortForListening();
+
+int WaitForClients();
 
 void InitServerSideNetwork(LogMessageFunctionPtr logInfo,
                            LogMessageFromPatternFunctionPtr logInfoFromPattern,
--- server/main.c
+++ server/main.c
@@ -8,56 +8,60 @@
 #include "../custom-libraries/myLogger/myLogger.h"
 #include "libraries/server_network/server_network.h"
 
+#define RUNNING_STATE 1
+
 int main() {
     InitMyLogger();
-
-    int client_socket;
-    struct sockaddr_in client_addr;
-    socklen_t addr_len = sizeof(client_addr);
-
     InitServerSideNetwork(LogInfo,
                           LogInfoFromPattern,
                           LogError,
                           LogErrorFromPattern);
 
-
-    while(1)
+    if(NETWORK_OPERATION_STATUS == FAILED)
     {
-        client_socket = accept(SERVER_SOCKET, (struct sockaddr*)&client_addr, &addr_len);
-        if(client_socket==-1)
+        exit(EXIT_FAILURE);
+    }
+
+    while(RUNNING_STATE)
+    {
+        int client_socket = WaitForClients();
+        if(NETWORK_OPERATION_STATUS == FAILED)
         {
-            LogError("Acceptance failed");
+            LogInfo("Client request denied. Server application will wait for another clients.");
+            continue;
         }
+        else
+        {
+            // Fork to handle the client
+            pid_t child_pid = fork();
 
-        // Fork to handle the client
-        pid_t child_pid = fork();
+            if (child_pid == 0) {
+                // This is the child process
+                close(SERVER_SOCKET); // Child doesn't need to listen
 
-        if (child_pid == 0) {
-            // This is the child process
-            close(SERVER_SOCKET); // Child doesn't need to listen
+                // Handle communication with the client
+                //handle_client(client_socket);
+                sleep(3);
+                char buffer[1024]; // Buffer to store received message
+                ssize_t bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
 
-            // Handle communication with the client
-            //handle_client(client_socket);
-            sleep(3);
-            char buffer[1024]; // Buffer to store received message
-            ssize_t bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
+                if (bytes_received <= 0) {
+                    LogError("Error receiving message");`
+                } else {
+                    buffer[bytes_received] = '\0'; // Add null terminator to make it a string
+                    LogInfoFromPattern("[Received message from client]: %s", buffer);
+                }
 
-            if (bytes_received <= 0) {
-                LogError("Error receiving message");
+                // Close client socket and exit child process
+                close(client_socket);
+                exit(EXIT_SUCCESS);
+            } else if (child_pid > 0) {
+                // This is the parent process
+                close(client_socket); // Parent doesn't need this socket
             } else {
-                buffer[bytes_received] = '\0'; // Add null terminator to make it a string
-                LogInfoFromPattern("[Received message from client]: %s", buffer);
+                LogError("Fork failed");
+                exit(EXIT_FAILURE);
             }
-
-            // Close client socket and exit child process
-            close(client_socket);
-            exit(EXIT_SUCCESS);
-        } else if (child_pid > 0) {
-            // This is the parent process
-            close(client_socket); // Parent doesn't need this socket
-        } else {
-            LogError("Fork failed");
-            exit(EXIT_FAILURE);
         }
     }
     //close(server_socket);
