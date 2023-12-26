e253822252d254be9e39e3fbba6d1d5c156a7e02
Cristian Roman <romancristianroman@yahoo.com>
2023-12-26 13:46:08 +0200

    bzero added for system_info

--- custom_libraries/system_info/system_info.c
+++ custom_libraries/system_info/system_info.c
@@ -1,6 +1,7 @@
 #include "system_info.h"
 
 #include <stdlib.h>
+#include <strings.h>
 #include <time.h>
 
 char* GetCurrentBuildTime()
@@ -11,6 +12,7 @@ char* GetCurrentBuildTime()
 
     const short CURRENT_BUILD_TIME_SIZE = 20;
     char* buildTime = malloc(CURRENT_BUILD_TIME_SIZE);
+    bzero(buildTime, CURRENT_BUILD_TIME_SIZE);
     strftime(buildTime, CURRENT_BUILD_TIME_SIZE, "%H:%M:%S", tm_info);
 
     return buildTime;
@@ -24,6 +26,7 @@ char* GetCurrentDate() {
 
     const short CURRENT_DATE_STRING_SIZE = 20;
     char* currentDate = malloc(CURRENT_DATE_STRING_SIZE);
+    bzero(currentDate, CURRENT_DATE_STRING_SIZE);
     strftime(currentDate, CURRENT_DATE_STRING_SIZE, "%Y-%m-%d", tm_info);
 
     return currentDate;
