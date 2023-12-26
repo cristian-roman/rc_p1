65dd2d5512bba127f88025a931bb8820896fbdf6
Cristian Roman <romancristianroman@yahoo.com>
2023-12-26 13:55:51 +0200

    added 0-ed cells for custom_c_string

--- custom_libraries/custom_c_string/custom_c_string.c
+++ custom_libraries/custom_c_string/custom_c_string.c
@@ -4,16 +4,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* CombineStrings(const int numStrings, const int total_size, ...) {
+char* CombineStrings(const int numStrings, int total_size, ...) {
 
-    char* result = malloc(total_size);
-    sprintf(result, "");
+    int writen_size = 0;
+    char* result = calloc(total_size, sizeof(char));
 
     va_list args;
     va_start(args, numStrings);
     for(int i = 0; i < numStrings; i++) {
         const char* str = va_arg(args, const char*);
-        strcat(result, str);
+        if(writen_size + strlen(str) > total_size) {
+            total_size *= 2;
+            result = realloc(result, total_size);
+        }
+        strcpy(result + writen_size, str);
+        writen_size += strlen(str);
     }
 
     va_end(args);
@@ -22,8 +27,7 @@ char* CombineStrings(const int numStrings, const int total_size, ...) {
 
 char* GetStringFromPattern(const char* pattern, const int total_size, ...) {
 
-    char* string = malloc(total_size);
-    sprintf(string, "");
+    char* string = calloc(total_size + 10, sizeof(char));
 
     va_list args;
     va_start(args, total_size);
